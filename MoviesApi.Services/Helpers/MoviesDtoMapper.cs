c8d7f3c729a7c721ebf44d68f0056fb67856ab56
Agostina <75433684+Agostina-mr@users.noreply.github.com>
2023-05-27 14:12:02 +0200

    add poster to MovieDto

--- MoviesApi.Services/Helpers/MoviesDtoMapper.cs
+++ MoviesApi.Services/Helpers/MoviesDtoMapper.cs
@@ -6,9 +6,11 @@ namespace MoviesApi.Services.Helpers;
 
 public static class MoviesDtoMapper
 {
+    private const string ImageUrl = "https://image.tmdb.org/t/p/w500";
+    
     public static MovieListDto ToMovieDto(this IEnumerable<FavoriteMovie> favoriteMovies, IReadOnlySet<int> favoriteIds)
     {
-        return new MovieListDto()
+        return new MovieListDto
         {
             Results = favoriteMovies.Select(fm => new MovieDto
             {
@@ -21,10 +23,10 @@ public static class MoviesDtoMapper
             }).ToList()
         };
     }
-
+    
     public static MovieListDto ToMovieDto(this MoviesResponseTmdb movies, IReadOnlySet<int> movieIds)
     {
-        return new MovieListDto()
+        return new MovieListDto
         {
             Results = movies.Results.Select(r => new MovieDto
             {
@@ -33,9 +35,12 @@ public static class MoviesDtoMapper
                 Description = r.Overview,
                 ReleaseDate = r.Release_date,
                 PosterPath = !string.IsNullOrEmpty(r.Poster_path)
-                    ? $"https://image.tmdb.org/t/p/w500{r.Poster_path}"
+                    ? $"{ImageUrl}{r.Poster_path}"
                     : null,
-                IsFavorite = movieIds.Contains(r.Id)
+                IsFavorite = movieIds.Contains(r.Id),
+                BackdropPath = !string.IsNullOrEmpty(r.Backdrop_path)
+                    ? $"{ImageUrl}{r.Backdrop_path}"
+                    : null,
             }).ToList()
         };
     }
@@ -49,7 +54,7 @@ public static class MoviesDtoMapper
             Overview = movie.Overview,
             Popularity = movie.Popularity,
             Poster_path = !string.IsNullOrEmpty(movie.Poster_path)
-                ? $"https://image.tmdb.org/t/p/w500{movie.Poster_path}"
+                ? $"{ImageUrl}{movie.Poster_path}"
                 : null,
             Release_date = movie.Release_date,
             Vote_average = movie.Vote_average,
@@ -71,7 +76,7 @@ public static class MoviesDtoMapper
 
     public static MovieCreditsResponseDto ToMovieCreditsDto(this MovieCreditsResponseTmdb credits)
     {
-        return new MovieCreditsResponseDto()
+        return new MovieCreditsResponseDto
         {
             Id = credits.Id,
             Cast = credits.Cast.Select(c => new MovieCastDto()
@@ -88,10 +93,10 @@ public static class MoviesDtoMapper
                 Original_name = c.Original_name,
                 Popularity = c.Popularity,
                 Profile_path = !string.IsNullOrEmpty(c.Profile_path)
-                    ? $"https://image.tmdb.org/t/p/w500{c.Profile_path}"
+                    ? $"{ImageUrl}{c.Profile_path}"
                     : null
             }).ToList(),
-            Crew = credits.Crew.Select(c => new MovieCrewDto()
+            Crew = credits.Crew.Select(c => new MovieCrewDto
             {
                 Adult = c.Adult,
                 Credit_id = c.Credit_id,
@@ -103,7 +108,7 @@ public static class MoviesDtoMapper
                 Original_name = c.Original_name,
                 Popularity = c.Popularity,
                 Profile_path = !string.IsNullOrEmpty(c.Profile_path)
-                    ? $"https://image.tmdb.org/t/p/w500{c.Profile_path}"
+                    ? $"{ImageUrl}{c.Profile_path}"
                     : null
             }).ToList()
         };
